17d6dfecc0447d258fd4a13a7da866840757873d
joaogabrielramos28 <joaogabrielramos28@outlook.com>
2022-01-06 19:33:10 -0300

    Limpando projeto next + configurações iniciais + configuração chackraui

---
+++ src/pages/theme/theme.ts
@@ -0,0 +1,17 @@
+import { extendTheme } from "@chakra-ui/react";
+
+const theme = extendTheme({
+  colors: {
+    gray: {
+      50: "#F5F8FA",
+      100: "#DADADA",
+      300: "#999999",
+      700: "#47585B",
+    },
+    yellow: {
+      700: "#FFBA08",
+    },
+  },
+});
+
+export default theme;
